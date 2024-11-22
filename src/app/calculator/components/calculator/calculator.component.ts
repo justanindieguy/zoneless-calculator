9e09e976b38eb3ea381e663cb3a47f921ea94b06
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-11-22 17:35:31 -0600

    configure calculator, calculator-view and app components with styles and routes

---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator.component.html',
+  styles: ``,
+})
+export class CalculatorComponent {}
